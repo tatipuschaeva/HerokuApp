33dee502c227b2feaf5cd093e7dd7770d00d23fb
Tatyana Pushchaeva <pushchaeva2000@mail.ru>
2024-12-03 19:36:03 +0500

    Added softAssert

--- src/test/java/AddRemoveElementsTest.java
+++ src/test/java/AddRemoveElementsTest.java
@@ -39,4 +39,3 @@ public class AddRemoveElementsTest {
         driver.quit();
     }
 }
-
--- src/test/java/InputsTest.java
+++ src/test/java/InputsTest.java
@@ -8,11 +8,14 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 import java.time.Duration;
 
 public class InputsTest {
 
     WebDriver driver;
+    SoftAssert softAssert = new SoftAssert();
 
     @BeforeMethod
     public void setup() {
@@ -28,25 +31,24 @@ public class InputsTest {
         WebElement inputField = driver.findElement(By.tagName("input"));
         inputField.clear();
         inputField.sendKeys("123");
-        Assert.assertEquals(inputField.getAttribute("value"), "123");// проверяем ввод цифр
+        softAssert.assertEquals(inputField.getAttribute("value"), "123");// проверяем ввод цифр
         inputField.clear();
         inputField.sendKeys("-123456");
-        Assert.assertEquals(inputField.getAttribute("value"), "-123456"); // проверяем возможность ввода допустимых символов
+        softAssert.assertEquals(inputField.getAttribute("value"), "-123456"); // проверяем возможность ввода допустимых символов
         inputField.clear();
         inputField.sendKeys("abc");
-        Assert.assertEquals(inputField.getAttribute("value"), ""); // проверяем, что доступен ввод только числовых значений
+        softAssert.assertEquals(inputField.getAttribute("value"), ""); // проверяем, что доступен ввод только числовых значений
         inputField.clear();
         inputField.sendKeys("1");
         inputField.sendKeys(Keys.ARROW_UP);
-        Assert.assertEquals(inputField.getAttribute("value"), "2");
+        softAssert.assertEquals(inputField.getAttribute("value"), "2");
         inputField.sendKeys(Keys.ARROW_DOWN);
-        Assert.assertEquals(inputField.getAttribute("value"), "1");
+        softAssert.assertEquals(inputField.getAttribute("value"), "1");
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit();
+        softAssert.assertAll();
     }
 }
-
-
